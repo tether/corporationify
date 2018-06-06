3e711dd3ed2b1cb693f516b6a27e2448bedaf188
bredele <olivier.wietrich@gmail.com>
2018-06-06 11:20:18 -0600

    Accept corporation type aliases

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 
+/**
+ * Corporation types aliases.
+ * @type {Object}
+ */
+
+const types = {
+  'Ltd': 'Limited',
+  'Inc': 'Incorporated'
+}
+
 module.exports = function (str) {
   const args = str
     .trim()
     .replace(/\s\s+/g, ' ')
-    .match(/(\w*)[\s]?[,.]?/)
+    .match(/(\w*)[\s]?[,.]?[\s]?(\w*)/)
+  const type = args[2] || 'Incorporated'
   return {
-    name: args[1]
+    name: args[1],
+    type: types[type] || type
   }
 }
--- test/corporationify.js
+++ test/corporationify.js
@@ -28,3 +28,33 @@ test('should get and trim corporation name from string ending containing a comma
   const {name} = extract('   Facebook  ,     ')
   assert.equal(name, 'Facebook')
 })
+
+test('should get and trim corporation name from string ending containing a dot', assert => {
+  assert.plan(1)
+  const {name} = extract('   Facebook    .     ')
+  assert.equal(name, 'Facebook')
+})
+
+test('should get default corporation type', assert => {
+  assert.plan(1)
+  const {type} = extract('   Facebook    .     ')
+  assert.equal(type, 'Incorporated')
+})
+
+test('should get given corporation type', assert => {
+  assert.plan(1)
+  const {type} = extract('   Blahblah    , Limited     ')
+  assert.equal(type, 'Limited')
+})
+
+test('should accept limited corporation type alias', assert => {
+  assert.plan(1)
+  const {type} = extract('Tether, Ltd')
+  assert.equal(type, 'Limited')
+})
+
+test('should accept incorporated corporation type alias', assert => {
+  assert.plan(1)
+  const {type} = extract('Tether, Inc')
+  assert.equal(type, 'Incorporated')
+})
